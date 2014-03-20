32c79318a548b48424bc9693aea0fdd2e6b548e5
Eli Gundry <eligundry@gmail.com>
2014-03-19 20:39:05 -0400

    Added db command to run.py

--- application/models.py
+++ application/models.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import subqueryload
 
 from application import db, bcrypt
 
-class User(db.model, UserMixin):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(254), unique=True, nullable=False)
--- run.py
+++ run.py
@@ -1,8 +1,10 @@
-from flask.ext.script import Manager
+import sys
+
+from flask.ext.script import Manager, prompt, prompt_bool, prompt_pass
 
 from application import create_app, db, models
 
-app = create_app('[application.settings.site]')
+app = create_app(['application.settings.site'])
 manager = Manager(app)
 
 @manager.shell
@@ -17,8 +19,20 @@ def make_shell_context():
 
 @manager.command
 def server():
+    "Run the server on localhost:5000"
     app.debug = True
     app.run()
+
+@manager.command
+def db(action):
+    "Create or delete the application's database"
+    if action is 'create':
+        pass
+    elif action is 'drop':
+        pass
+    else:
+        print(u'Invalid action "{}"'.format(action))
+        sys.exit(1)
 
 if __name__ == "__main__":
     manager.run()
